93025e7a454fce552a167777f84ff45634d12e59
Carlos <mugetsuvita@gmail.com>
2022-07-14 11:18:21 +0600

    Database

---
+++ backend/dsmeta/src/main/java/dev/carlvs/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,11 @@
+package dev.carlvs.dsmeta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dev.carlvs.dsmeta.domain.Sale;
+
+@Repository
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+    
+}
